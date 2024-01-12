506ae8c6dbd6862b3ee38e11dc5c82c021e67cab
shilpagoka <156395151+shilpagoka@users.noreply.github.com>
2024-01-11 22:49:45 -0500

    Create SortColors.py

---
+++ code/Arrays/SortColors.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        for i in range(len(nums)):
+            for j in range(i,len(nums)):
+                if nums[i] > nums[j]:
+                    nums[i], nums[j] = nums[j], nums[i]
+    #Time complexity = O(n^2)
+    #Space complexity = O(1)
